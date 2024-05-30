909e557404445c3033b1af8c2923bee61514e712
Nils Henrar <nils.henrar@gmail.com>
2024-05-30 11:36:26 +0200

    page d'accueil présentation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,17 +4,28 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+            <div class="card" style="margin-top: 20px; text-align: center;">
 
                 <div class="card-body">
-                    @if (session('status'))
+                    <!-- @if (session('status') == 'verification-link-sent')
                     <div class="alert alert-success" role="alert">
                         {{ session('status') }}
                     </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('You are logged in!') }} -->
+
+                    <h1 class="text-3xl font-bold mb-8">Bienvenue sur le site de réservation de spectacles</h1>
+                    <p class="text-lg">Vous pouvez consulter les spectacles disponibles</p>
+
+                    <!-- si je ne suis pas connecté -->
+                    @guest
+                    <p class="text-lg">Connectez-vous pour accéder à votre espace personnel et pouvoir réserver des spectacles</p>
+                    <div class="mt-4">
+                        <a href="{{ route('login') }}" class="text-blue-500 hover:text-blue-700">Se connecter</a>
+                    </div>
+                    @endguest
+
                 </div>
             </div>
         </div>
